570302eb2fcc9e2fd19994d3a0f8d8f40a2eeeae
mana-mama <122524277+mana-mama@users.noreply.github.com>
2024-11-12 15:52:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let mode = 0;
+function SwitchLightDarkMode() {
+    if (mode == 0) {
+        document.querySelector(':root').style.setProperty('--color_1', "#B9E5E8");
+        document.querySelector(':root').style.setProperty('--color_3', "#DFF2EB");
+        document.querySelector(':root').style.setProperty('--color_4', "#4A4947");
+        document.querySelector(':root').style.setProperty('--color_5', "#000");
+        document.querySelector(':root').style.setProperty('--color_6', "#fff");
+        mode = 1;
+        document.querySelector('.fa-sun').classList.remove('active');
+        document.querySelector('.fa-moon').classList.toggle('active');
+    } else {
+        document.querySelector(':root').style.setProperty('--color_1', "#7286D3");
+        document.querySelector(':root').style.setProperty('--color_3', "#E5E0FF");
+        document.querySelector(':root').style.setProperty('--color_4', "#FFF2F2");
+        document.querySelector(':root').style.setProperty('--color_5', "#fff");
+        document.querySelector(':root').style.setProperty('--color_6', "#000");
+        mode = 0;
+        document.querySelector('.fa-sun').classList.toggle('active');
+        document.querySelector('.fa-moon').classList.remove('active');
+    }
+}
+
+function imgFull(img) {
+    img.classList.toggle('img-box-active');
+}
+
+function paperFull(img) {
+    img.classList.toggle('paper-con-active');
+}
