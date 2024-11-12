662908a0bd780e73ffa6d8409afd4288e7a7114f
mana-mama <122524277+mana-mama@users.noreply.github.com>
2024-11-12 16:07:42 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -30,8 +30,14 @@ function SwitchLightDarkMode() {
 
 function imgFull(img) {
     img.classList.toggle('img-box-active');
+    setTimeout (function () {
+        img.classList.remove('img-box-active');
+    }, 2000) 
 }
 
 function paperFull(img) {
     img.classList.toggle('paper-con-active');
+    setTimeout (function () {
+        img.classList.remove('paper-con-active');
+    }, 2000) 
 }
